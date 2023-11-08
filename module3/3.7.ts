772b000150190bd5233a913ccc0e72fd6d3ace93
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-11-09 00:48:32 +0600

    statics in OOP

---
+++ module3/3.7.ts
@@ -0,0 +1,30 @@
+{
+    // statics in OOP
+
+    class Counter {
+        static count: number = 0;
+        static increment() {
+            return (Counter.count = Counter.count + 1)
+        }
+
+        static decrement() {
+            return (Counter.count = Counter.count - 1)
+        }
+    }
+
+
+    // const instance1 = new Counter();
+    console.log(Counter.increment());
+
+    // const instance2 = new Counter();
+    console.log(Counter.increment());
+
+    // const instance3 = new Counter();
+    console.log(Counter.increment());
+
+
+
+
+
+    //
+}
