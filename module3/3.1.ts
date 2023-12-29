5d418c9c76d1bef46db90c18d2d42e21083f9794
Tamim Iqbal Maruf <marufmd775@gmail.com>
2023-12-29 21:06:14 +0600

    Parameter Properties Public

--- module3/3.1.ts
+++ module3/3.1.ts
@@ -1,17 +1,41 @@
 {
     // class and object
     class Animal {
-        // name: string;
-        // species: string;
-        // sound: string;
+        name: string;
+        species: string;
+        sound: string;
 
         //Parameter Properties
-        constructor(public name: string, public species: string, public sound: string) {
-            // this.name = name;
-            // this.species = species;
-            // this.sound = sound;
+        constructor(name: string, species: string, sound: string) {
+            this.name = name;
+            this.species = species;
+            this.sound = sound;
         }
 
+        // method
+        makeSound() {
+            console.log(`The ${this.name} says ${this.sound}`);
+        }
+    }
+
+    const dog = new Animal("German kup vai", "Dog", "kupi kupi");
+
+    const cat = new Animal("Mia Cat", "Cat", "Mew mew")
+
+    cat.makeSound();
+    //
+}
+
+
+
+{
+    // class and object
+    class Animal {
+
+        //Parameter Properties Public
+        constructor(public name: string, public species: string, public sound: string) { }
+
+        // method
         makeSound() {
             console.log(`The ${this.name} says ${this.sound}`);
         }
